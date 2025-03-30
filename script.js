814bb38fdd127aaf67f793d9050d11b3e80f289c
sarawone <sarawone.poudal@gmail.com>
2025-03-30 13:00:17 +0100

    adding function

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 //You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  // get all episodes array & loop each one
+  const allEpisodes = getAllEpisodes().map(makePageForEpisodes);
+  
+  //append all the return value to the body
+  document.body.append(...allEpisodes);
+
 }
 
+
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  //clone template 
+  const movTemplate = document.getElementById("card").content.cloneNode(true);
+  
+  // adding title 
+  const episodeTitle  = movTemplate.querySelector("h3");
+
+  // padding 0 infront of season 
+  const epiSeason = "S"+String(episodeList.season).padStart(2,"0");
+  //adding 0 inforont of the episode number 
+  const epiNumber = "E"+String (episodeList.number).padStart(2,"0"); 
+  episodeTitle.textContent = episodeList.name + '-' + epiSeason + epiNumber;
+
+  //adding image 
+  const episodePic  =  movTemplate.querySelector("img");
+  episodePic.src = episodeList.image.medium;
+
+  //adding info 
+  const episodeSummary = movTemplate.querySelector("p");
+  episodeSummary.innerHTML = episodeList.summary;
+
+
+ return movTemplate;
+
 }
+
+
+
+
 
 window.onload = setup;
